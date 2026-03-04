ea237e0f34c2fa4707dcc2d58e9707394b790a86
UtakataKyosui <fill.ayaextech@gmail.com>
2026-03-05 00:38:05 +0900

    feat: Tiptapの導入

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import './App.css';
+import Tiptap from './tiptap/tiptap-app';
 
 const App = () => {
   return (
-    <div className="content">
-      <h1>Rsbuild with React</h1>
-      <p>Start building amazing things with Rsbuild.</p>
-    </div>
+    <Tiptap />
   );
 };
 
